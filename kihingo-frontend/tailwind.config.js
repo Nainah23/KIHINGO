e947177105c8622ea7a7e241cb3c2735339c6646
Nainah23 <133117208+Nainah23@users.noreply.github.com>
2024-12-30 16:49:03 +0300

    refactor: Update Tailwind CSS configuration and dependencies

--- kihingo-frontend/tailwind.config.js
+++ kihingo-frontend/tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        scroll: 'scroll 15s linear',
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('autoprefixer'),
   ],
 }
+
